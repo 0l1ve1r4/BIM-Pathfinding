f3b20985b93d06d3d0397155e80f6f8c96db6884
iyksh <gssantoz2012@gmail.com>
2024-04-05 10:25:14 -0300

    Auto commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@
 #                       just click in "New Image" button on the GUI.
 # ==============================================================================
 
-from mainGUI import *
+#from mainGUI import *
 from utils import *
 
 import os
@@ -16,7 +16,7 @@ import json
 
 if __name__ == "__main__":
     print("\033[H\033[J")
-    check_and_install_libraries()
+    #check_and_install_libraries()
     
     run_path = os.getcwd()
     config_file = "src/config.json"
@@ -35,6 +35,13 @@ if __name__ == "__main__":
         with open(config_file, 'r') as f:
             config_data = json.load(f)
 
+    os.system('git add .')
+    os.system('git commit -m "Auto commit"')
+    os.system('git push origin main')
+    os.system('git pull origin main')
+    
+    """
     root = tk.Tk()
     app = MatrixGUI(root, config_data["config"]["default_matrix_path"], os_name=os_name)
     root.mainloop()
+    """
